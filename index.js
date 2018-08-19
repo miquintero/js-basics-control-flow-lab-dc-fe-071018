c84cc52fbdff5980d13b5d5a0e3676c700e3ca9d
Alexa Schaeffer <alps231@gmail.com>
2018-08-19 20:52:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
     return "This one is on me!";
   }
-  else if (199 < feet && feet < 2500) {
+  else if (1999 < feet && feet < 2500) {
     return "I will gladly take your thirty bucks.";
   }
   else {
