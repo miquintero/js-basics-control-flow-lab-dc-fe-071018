b6a27e1cdafb9468ea06d0aa82831d17f038a76b
Alexa Schaeffer <alps231@gmail.com>
2018-08-19 20:46:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,18 +12,27 @@ function scuberGreetingForFeet(distance) {
 }
 
 function ternaryCheckCity(city) {
-  console.log)"== ternaryCheckCity ==");
-  let destination = (city === "NYC")
-    ? "Ok, sounds good."
-    : "No, go."
+  console.log("== ternaryCheckCity ==");
+  let destination = city === "NYC" ? "Ok, sounds good." : "No, go."
   console.log("destination", destination);
   return destination;
 }
 
 function switchOnCharmFromTip(tip) {
   console.log("== switchOnCharmFromTip ==");
-
-  switch
+  let tip; 
+  switch (amount) {
+    case "stingy":
+      tip <= 2;
+      break; 
+    case "reasonable": 
+      tip >= 7;
+      break;
+    case "generous":
+      tip > 11; 
+      break; 
+    
+  }
 
 
   }
