3469a4f249c5ec6e47d30c47e17c53866892446f
Alexa Schaeffer <alps231@gmail.com>
2018-08-19 20:38:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(distance) {
   if (distance <= 400) {
     return "This one is on me!";
   }
-  if (distance >= 2000) {
+  else if (distance >= 2000) {
     return "I will gladly take your thirty bucks.";
   }
   no (distance > 2500) {
