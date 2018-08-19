ceddf8137785098ba0b91c837e8a9ab387a50221
Alexa Schaeffer <alps231@gmail.com>
2018-08-19 20:34:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  console.log("==scuberGreetingForFeet ==")
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  if (distance >= 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+  no (distance > 2500) {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city) {
+  console.log)"== ternaryCheckCity ==");
+  let destination = (city === "NYC")
+    ? "Ok, sounds good."
+    : "No, go."
+  console.log("destination", destination);
+  return destination;
+}
+
+function switchOnCharmFromTip(tip) {
+  console.log("== switchOnCharmFromTip ==");
+
+  switch
+
+
+  }
+}
